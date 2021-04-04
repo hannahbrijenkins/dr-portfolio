dfb1a2362b626551d4ca15cbffcbf9681dc19f5f
Hannah Jenkins <hannahbri@hannahs-air.home>
2021-04-03 21:41:35 -0400

    adjusted nav - still needs work

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function menuFunction(x) {
     x.classList.toggle("change")
+
+    var y = document.getElementById("myTopnav");
+    if (y.className === "topnav") {
+        y.className += " responsive";
+    } else {
+        y.className = "topnav";
+    }
 };
